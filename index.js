5b9f83c1dbe49f9e79f80654f369d2df2cb02a8d
Jerome Covington <jeromecovington@gmail.com>
2018-11-19 22:15:08 -0500

    init test case

---
+++ index.js
@@ -0,0 +1,29 @@
+const Hapi = require('hapi');
+const rp = require('request-promise');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/asking-for-a-friend',
+    handler: (request, h) => {
+      const proxyUrl = `https://duckduckgo.com/${request.url.search}`;
+      console.log({ proxyUrl });
+      return rp(proxyUrl);
+    }
+});
+
+const init = async () => {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
